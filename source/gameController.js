60bd20ab5267a68b83d9086a5acc0317f82e2705
shaendro <p.lonauer@gmail.com>
2017-05-09 00:47:42 +0200

    const-ified the gameController

--- source/gameController.js
+++ source/gameController.js
@@ -46,27 +46,27 @@ function ask(req, res) {
 };
 
 function getQuestion(req, res) {
-    let gameId = req.params.gameId;
+    const gameId = req.params.gameId;
     if (games[gameId] == undefined) return res.json({ 'success': false, 'error': 'Game not found.' });
-    let game = games[gameId];
-    let round = game.rounds[game.roundId];
+    const game = games[gameId];
+    const round = game.rounds[game.roundId];
     if (round == undefined || round.end < new Date()) return res.json({ 'success': false, 'error': 'No question available.' });
-    let remainingTime = round.end.getTime() - new Date().getTime();
+    const remainingTime = round.end.getTime() - new Date().getTime();
     return res.json({ 'success': true, 'round': game.roundId, 'question': quizController.getQuestion(round.question), 'end': remainingTime });
 };
 
 function answer(req, res) {
-    let gameId = req.body.gameId;
-    let roundId = req.body.roundId;
-    let answer = req.body.answer;
-    let username = req.body.username;
+    const gameId = req.body.gameId;
+    const roundId = req.body.roundId;
+    const answer = req.body.answer;
+    const username = req.body.username;
     if (games[gameId] == undefined) return res.json({ 'success': false, 'error': 'Game not found.' });
     if (games[gameId].rounds[roundId] == undefined) return res.json({ 'success': false, 'error': 'Round not found.' });
     if (games[gameId].users[username] == undefined) return res.json({ 'success': false, 'error': 'User not found.' });
-    let game = games[gameId];
-    let round = game.rounds[roundId];
+    const game = games[gameId];
+    const round = game.rounds[roundId];
     if (round.end < new Date()) return res.json({ 'success': false, 'error': 'Round over.' });
-    let question = quizController.getQuestion(round.question);
+    const question = quizController.getQuestion(round.question);
     if (games[gameId].users[username].round >= roundId) return res.json({ 'success': false, 'error': 'User has already answered.' });
     if (question.type == 'choice' && !['a', 'b', 'c', 'd'].includes(answer)) return res.json({ 'success': false, 'error': 'Wrong answer format.' });
     if (question.type == 'estimate' && isNaN(answer)) return res.json({ 'success': false, 'error': 'Answer was not a number.' });
@@ -98,40 +98,40 @@ function answer(req, res) {
 };
 
 function resultQuiz(req, res) {
-    let gameId = req.params.gameId;
-    let roundId = req.params.roundId;
+    const gameId = req.params.gameId;
+    const roundId = req.params.roundId;
     if (games[gameId] == undefined) return res.json({ 'success': false, 'error': 'Game not found.' });
     if (games[gameId].rounds[roundId] == undefined) return res.json({ 'success': false, 'error': 'Round not found.' });
-    let round = games[gameId].rounds[roundId];
+    const round = games[gameId].rounds[roundId];
     if (round.end > new Date()) return res.json({ 'success': false, 'error': 'Round not over.' });
-    let type = quizController.getQuestion(round.question).type;
-    let answer = quizController.getAnswer(round.question);
+    const type = quizController.getQuestion(round.question).type;
+    const answer = quizController.getAnswer(round.question);
     return res.json({ 'success': true, 'answer': answer, 'result': round });
 };
 
 function resultAction(req, res) {
-    let gameId = req.params.gameId;
-    let roundId = req.params.roundId;
+    const gameId = req.params.gameId;
+    const roundId = req.params.roundId;
     if (games[gameId] == undefined) return res.json({ 'success': false, 'error': 'Game not found.' });
     if (games[gameId].rounds[roundId] == undefined) return res.json({ 'success': false, 'error': 'Round not found.' });
-    let round = games[gameId].rounds[roundId];
+    const round = games[gameId].rounds[roundId];
     if (round.end > new Date()) return res.json({ 'success': false, 'error': 'Round not over.' });
     return res.json({ 'success': true, 'result': round.correct == 0 ? 0 : round.correct / round.participants });
 };
 
 function scoreboard(req, res) {
-    let gameId = req.params.gameId;
+    const gameId = req.params.gameId;
     if (games[gameId] == undefined) return res.json({ 'success': false, 'error': 'Game not found.' });
-    let users = games[gameId].users;
-    let scoreboard = {};
-    for (let key in users) {
+    const users = games[gameId].users;
+    const scoreboard = {};
+    for (const key in users) {
         scoreboard[key] = users[key].score;
     };
     return res.json({ 'success': true, 'scoreboard': scoreboard });
 };
 
 function end(req, res) {
-    let gameId = req.body.gameId;
+    const gameId = req.body.gameId;
     if (games[gameId] == undefined) return res.json({ 'success': false, 'error': 'Game not found.' });
     games[gameId] = undefined;
     return res.json({ 'success': true });
