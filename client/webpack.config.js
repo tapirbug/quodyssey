82af70436a610bbd5f8f3290ce07efe5be6a1d95
Paul Lonauer <p.lonauer@gmail.com>
2017-05-15 10:04:27 +0200

    restructuring of client code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
       quizplayer: "./quizplayer.js"
   },
   output: {
-    path: __dirname + "/dist",
+    path: __dirname + "/../public/dist",
       // Make sure to use [name] or [id] in output.filename
       //  when using multiple entry points
       filename: "[name].bundle.js",
