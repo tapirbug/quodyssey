9c91b7e360e22076de749743be5636b077645e64
krachzack <hello@phstadler.com>
2017-06-07 17:04:29 +0200

    Added long polling

--- client/fayeClient.js
+++ client/fayeClient.js
@@ -2,14 +2,49 @@ const faye = require('faye');
 
 const client = new faye.Client(`http://localhost:3333/channel/`);
 
-function register(channel) {
+function register(channel, callbackQuestion, callbackScoreboard) {
     client.subscribe(`/${channel}`, notification => {
         if (notification.type === 'question') {
             console.log('Got a new question!');
             console.log(notification.data);
+            if(callbackQuestion) {
+              console.log({
+                  id: notification.data.question.id,
+                  round: notification.data.round,
+                  type: notification.data.question.type,
+                  prompt: notification.data.question.question,
+                  options: [
+                      notification.data.question.a,
+                      notification.data.question.b,
+                      notification.data.question.c,
+                      notification.data.question.d
+                  ],
+                  duration: notification.data.end,
+                  end: new Date((new Date()).getTime() + notification.data.end),
+                  start: new Date(),
+              })
+              callbackQuestion({
+                  id: notification.data.question.id,
+                  round: notification.data.round,
+                  type: notification.data.question.type,
+                  prompt: notification.data.question.question,
+                  options: [
+                      notification.data.question.a,
+                      notification.data.question.b,
+                      notification.data.question.c,
+                      notification.data.question.d
+                  ],
+                  duration: notification.data.end,
+                  end: new Date((new Date()).getTime() + notification.data.end),
+                  start: new Date(),
+              })
+            }
         } else if (notification.type === 'scoreboard') {
             console.log('Got a scoreboard!');
             console.log(notification.data);
+            if(callbackScoreboard) {
+              callbackScoreboard(notification.data)
+            }
         }
     });
 }
